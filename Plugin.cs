333ce3ecdcfed51026f50e44cd464b6c9b6becb7
ChiHaoLu <b08303113@ntu.edu.tw>
2023-12-23 03:32:36 +0800

    build project

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ModChecksumPlugin
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Lethal Company.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static ManualLogSource logger;
+        private void Awake()
+        {
+            // Plugin startup logic
+            logger = Logger;
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_NAME);
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo(GetModCode());
+        }
+
+        private static string GetModCode()
+        {
+            string code = "";
+            foreach (var plugin in Chainloader.PluginInfos)
+            {
+                code += plugin.ToString();
+            }
+            return code;
+        }
+    }
+}
---
+++ obj/Debug/net461/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ModChecksumPlugin
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ModChecksumPlugin";
+        public const string PLUGIN_NAME = "ModChecksumPlugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
